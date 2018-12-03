51dab327bae67f1a85cf2d3d655e630aa2669c50
Masashi Otsuka <masashi1029xxx@gmail.com>
2018-12-04 08:24:05 +0900

    Update gen_RegString.py

--- gen_RegString.py
+++ gen_RegString.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#import csv
-import numpy as np
-
 d_Len = 254;
 
 
 def out_reg_str():
-   x = d_Len;
-   list = [];
-   num = 0;
-   for i in range (x):
-      list.append(num) if num > 10 else num = 0
+   x = d_Len
+   list = []
+   num = 0
+   for i in range(x):
+       if num < 10:
+        list.append(num)
+        num += 1
+        print num
+       else:
+        num = 0
+        print num
+        list.append(num)
+
+    map(str,list)
+    reg_str = ','.join(list)
+    print reg_str
 
 print out_reg_str();
